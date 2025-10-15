d94807b9600e0071d1391fcf416a3481685f3aa7
Diego Espindola <diegoespindola9404@gmail.com>
2025-10-15 19:00:31 -0300

    Testando listagem de transações

--- 02-CRIANDO-API-REST/test/example.spec.ts
+++ 02-CRIANDO-API-REST/test/example.spec.ts
@@ -1,7 +1,6 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, expect, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
-import { describe } from 'node:test'
 
 
 describe('Transactions routes', () => {
@@ -14,7 +13,8 @@ describe('Transactions routes', () => {
         await app.close()
     })
 
-    test('use can create a new transaction', async () => {
+
+    it('should be able to create a new transaction', async () => {
         await request(app.server)
             .post('/transactions')
             .send({
@@ -23,6 +23,34 @@ describe('Transactions routes', () => {
                 type: 'credit',
             })
             .expect(201)
-    })
+    });
+
+    it('should be able to list all transactions', async () => {
+        const postResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit',
+            });
+
+        const cookies = postResponse.get('Set-Cookie') as string[];
+
+        const response = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200);
+        console.log(response.body)
+        expect(response.body).toHaveProperty('transactions');
+        expect(response.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            })
+        ]
+        );
+        expect(Array.isArray(response.body.transactions)).toBe(true);
+
+    });
 
 });
