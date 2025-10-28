aae5c0273283bc6b4116d924c75a00e5d61f382d
Diego Espindola <diegoespindola9404@gmail.com>
2025-10-28 11:26:59 -0300

    separar a lógica do http

--- 03-API-SOLID/src/http/controllers/register.controller.ts
+++ 03-API-SOLID/src/http/controllers/register.controller.ts
@@ -1,24 +1,22 @@
 import { z } from 'zod'
-import { prisma } from '../../lib/prisma.js'
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { registerUseCase } from '../use-cases/register.usecase.js';
 
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    });
 
-  const { name, email, password } = registerBodySchema.parse(request.body)
+    const { name, email, password } = registerBodySchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
+    try {
+        await registerUseCase({ name, email, password })
+    } catch (err) {
+        return reply.status(409).send();
+    }
 
-  return reply.status(201).send()
+    return reply.status(201).send();
 }
