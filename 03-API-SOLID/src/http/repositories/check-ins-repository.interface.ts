af5eb5432260bd58f2190100eaeed5abf57d73e7
Diego Espindola <diegoespindola9404@gmail.com>
2025-11-01 14:57:18 -0300

    Implementando casos de uso - TDD & Mocking

--- 03-API-SOLID/src/http/repositories/check-ins-repository.interface.ts
+++ 03-API-SOLID/src/http/repositories/check-ins-repository.interface.ts
@@ -1,5 +1,6 @@
 import { type CheckIn, Prisma } from "generated/prisma/index.js";
 
 export interface ICheckInsRepository {
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
 }
