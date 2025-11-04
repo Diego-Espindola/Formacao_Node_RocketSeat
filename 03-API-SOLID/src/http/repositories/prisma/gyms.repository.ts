20efc46e1c922ec6eef68c11e79e90bc4f6e5dcf
Diego Espindola <diegoespindola9404@gmail.com>
2025-11-04 16:12:39 -0300

    Repositório de academias

--- 03-API-SOLID/src/http/repositories/prisma/gyms.repository.ts
+++ 03-API-SOLID/src/http/repositories/prisma/gyms.repository.ts
@@ -1,19 +1,72 @@
 import { prisma } from '../../../lib/prisma.js'
 import { Prisma, type Gym } from 'generated/prisma/index.js';
 import type { FindManyNearbyParams, IGymsRepository } from '../gyms-repository.interface.js';
+import { getDistanceBetweenCoordinates } from '@/http/utils/get-distance-between-coordinates.js';
 
 export class PrismaUsersRepository implements IGymsRepository {
-  findById(id: string): Promise<Gym | null> {
-    throw new Error('Method not implemented.');
+  public async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id
+      }
+    });
+
+    return gym;
   }
-  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]> {
-    throw new Error('Method not implemented.');
+
+  public async findManyNearby(params: FindManyNearbyParams) {
+    const RADIUS_IN_DEGREES = 0.001; // ~111m de latitude
+    const gyms = await prisma.gym.findMany({
+      where: {
+        latitude: {
+          gte: params.latitude - RADIUS_IN_DEGREES,
+          lte: params.latitude + RADIUS_IN_DEGREES,
+        },
+        longitude: {
+          gte: params.longitude - RADIUS_IN_DEGREES,
+          lte: params.longitude + RADIUS_IN_DEGREES,
+        },
+      },
+    });
+
+    const LIMIT_METERS_TO_NEARBY = 10;
+    const nearbyGyms = gyms.filter(gym =>
+      getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+      ) <= LIMIT_METERS_TO_NEARBY
+    );
+
+    return nearbyGyms;
   }
-  searchMany(query: string, page: number): Promise<Gym[]> {
-    throw new Error('Method not implemented.');
+
+  public async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        OR: [
+          {
+            description: {
+              contains: query
+            }
+          },
+          {
+            title: {
+              contains: query
+            }
+          }
+        ]
+      },
+      take: 20,
+      skip: (page - 1) * 20
+    });
+
+    return gyms;
   }
-  create(data: Prisma.GymCreateInput): Promise<Gym> {
-    throw new Error('Method not implemented.');
+
+  public async create(data: Prisma.GymCreateInput) {
+    return await prisma.gym.create({
+      data
+    })
   }
 
 }
