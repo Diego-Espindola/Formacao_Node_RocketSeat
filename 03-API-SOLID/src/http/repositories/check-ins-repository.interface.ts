336f66c1d3d35700aeafe15e7031e476644ea4c3
Diego Espindola <diegoespindola9404@gmail.com>
2025-11-04 08:20:44 -0300

    Caso de uso de histórico

--- 03-API-SOLID/src/http/repositories/check-ins-repository.interface.ts
+++ 03-API-SOLID/src/http/repositories/check-ins-repository.interface.ts
@@ -2,5 +2,6 @@ import { type CheckIn, Prisma } from "generated/prisma/index.js";
 
 export interface ICheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
 }
---
+++ 03-API-SOLID/src/http/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { type ICheckInsRepository } from "../repositories/check-ins-repository.interface.js"
+import { type CheckIn } from "generated/prisma/index.js"
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: ICheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
