d2725921e425d2df55fe8f9447ac7bb6a3213658
Diego Espindola <diegoespindola9404@gmail.com>
2025-11-10 18:41:04 -0300

    Autorização por cargos (RBAC)

--- 03-API-SOLID/src/http/controllers/gyms/routes.ts
+++ 03-API-SOLID/src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt.js'
 import { search } from "./search.controller.js";
 import { nearby } from "./nearby.controller.js";
 import { create } from "./create.controller.js";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role.js";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search);
   app.get('/gyms/nearby', nearby);
 
-  app.post('/gyms', create);
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create);
 }
