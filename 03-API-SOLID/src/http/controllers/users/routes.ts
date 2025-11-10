c2c43e90782fefbe71e1ee070756a8f83a644284
Diego Espindola <diegoespindola9404@gmail.com>
2025-11-09 21:57:46 -0300

    Implementação do refresh token

--- 03-API-SOLID/src/http/controllers/users/routes.ts
+++ 03-API-SOLID/src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from "./register.controller.js";
 import { authenticate } from "./authenticate.controller.js";
 import { profile } from "./profile.controller.js";
 import { verifyJwt } from "@/http/middlewares/verify-jwt.js";
+import { refresh } from "./refresh.controller.js";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
