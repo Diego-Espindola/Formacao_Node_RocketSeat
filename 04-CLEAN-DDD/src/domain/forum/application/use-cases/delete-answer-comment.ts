75cc71978d49fa2409ab0d3e11b1c982eded4085
Diego Espindola <diegoespindola9404@gmail.com>
2025-12-20 09:24:11 -0300

    either

--- 04-CLEAN-DDD/src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ 04-CLEAN-DDD/src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { left, right, type Either } from '@/core/types/either.js'
 import { type AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository.js'
 
 interface DeleteAnswerCommentUseCaseRequest {
@@ -5,7 +6,7 @@ interface DeleteAnswerCommentUseCaseRequest {
   answerCommentId: string
 }
 
-interface DeleteAnswerCommentUseCaseResponse {}
+type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
@@ -19,15 +20,15 @@ export class DeleteAnswerCommentUseCase {
     )
 
     if (!answerComment) {
-      throw new Error('Answer comment not found.')
+      return left('Answer comment not found.');
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed')
+      return left('Not allowed');
     }
 
     await this.answerCommentsRepository.delete(answerComment)
 
-    return {}
+    return right({});
   }
 }
