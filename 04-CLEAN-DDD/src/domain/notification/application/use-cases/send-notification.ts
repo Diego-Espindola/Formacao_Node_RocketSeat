fa979e1c4b585c2a6ac2ae66873f446fed129617
Diego Espindola <diegoespindola9404@gmail.com>
2026-03-05 18:12:28 -0300

    Subdomínios & Domain Events - Caso de uso: envio de notificação

---
+++ 04-CLEAN-DDD/src/domain/notification/application/use-cases/send-notification.ts
@@ -0,0 +1,39 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { type Either, right } from '@/core/types/either.js'
+import { Notification } from '../../enterprise/entities/notification.js'
+import { type NotificationsRepository } from '../repositories/notifications-repository.js'
+
+interface SendNotificationUseCaseRequest {
+  recipientId: string
+  title: string
+  content: string
+}
+
+type SendNotificationUseCaseResponse = Either<
+  null,
+  {
+    notification: Notification
+  }
+>
+
+export class SendNotificationUseCase {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute({
+    recipientId,
+    title,
+    content,
+  }: SendNotificationUseCaseRequest): Promise<SendNotificationUseCaseResponse> {
+    const notification = Notification.create({
+      recipientId: new UniqueEntityID(recipientId),
+      title,
+      content,
+    })
+
+    await this.notificationsRepository.create(notification)
+
+    return right({
+      notification,
+    })
+  }
+}
