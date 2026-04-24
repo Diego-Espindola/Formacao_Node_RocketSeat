840cc3c499798bc70fc80545844c9b363416918f
Diego Espindola <diegoespindola9404@gmail.com>
2026-04-24 16:22:27 -0300

    Fundamentos do Nest - Configurando Vitest com SWC

--- 05-NEST-CLEAN/src/controllers/create-question.controller.ts
+++ 05-NEST-CLEAN/src/controllers/create-question.controller.ts
@@ -5,13 +5,13 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { PrismaService } from '@/prisma/prisma.service'
 import { hash } from 'bcryptjs'
 import { z } from 'zod'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { CurrentUser } from 'src/auth/current-user.decorator'
-import { type UserTokenPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { CurrentUser } from '@/auth/current-user.decorator'
+import { type UserTokenPayload } from '@/auth/jwt.strategy'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
