716497b4f411648083b7f9ec643c1842cb87db91
Diego Espindola <diegoespindola9404@gmail.com>
2026-04-24 16:28:33 -0300

    de dados isolado nos testes

---
+++ 05-NEST-CLEAN/test/setup-e2e.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from 'prisma/generated/prisma/client'
+
+let prisma: PrismaClient
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  prisma = new PrismaClient({
+    adapter: new PrismaPg(databaseURL),
+    log: ['warn', 'error'],
+  })
+
+  execSync('npx prisma migrate deploy', {
+    stdio: 'inherit',
+    env: { ...process.env, DATABASE_URL: databaseURL },
+  })
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
