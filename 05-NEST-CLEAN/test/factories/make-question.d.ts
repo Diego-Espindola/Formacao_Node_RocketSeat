f6f7f96cb071f1e73653578ebc956e48510201c0
Diego Espindola <diegoespindola9404@gmail.com>
2026-04-30 16:35:11 -0300

    Testes do cadastro e autenticação

--- 05-NEST-CLEAN/test/factories/make-question.d.ts
+++
@@ -1,3 +0,0 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id.js';
-import { Question, type QuestionProps } from '@/domain/forum/enterprise/entities/question.js';
-export declare function makeQuestion(override?: Partial<QuestionProps>, id?: UniqueEntityID): Question;
