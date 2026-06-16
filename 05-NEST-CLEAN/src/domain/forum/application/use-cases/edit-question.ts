bdafaed29be38e762a534783d633d8df477b45ee
Diego Espindola <diegoespindola9404@gmail.com>
2026-06-16 18:34:28 -0300

    Controller: Editar Pergunta

--- 05-NEST-CLEAN/src/domain/forum/application/use-cases/edit-question.ts
+++ 05-NEST-CLEAN/src/domain/forum/application/use-cases/edit-question.ts
@@ -1,12 +1,13 @@
 import { left, right, type Either } from '@/core/types/either.js'
-import type { Question } from '../../enterprise/entities/question.js'
-import { type QuestionsRepository } from '../repositories/questions-repository.js'
+import { Question } from '../../enterprise/entities/question.js'
+import { QuestionsRepository } from '../repositories/questions-repository.js'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found.error.js'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed.error.js'
-import type { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository.js'
+import { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository.js'
 import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list.js'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment.js'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { Injectable } from '@nestjs/common'
 
 interface EditQuestionUseCaseRequest {
   authorId: string
@@ -23,6 +24,7 @@ type EditQuestionUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class EditQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
